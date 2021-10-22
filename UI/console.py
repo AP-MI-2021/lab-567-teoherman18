bcb333a77fc99999922922bdec6fe1e927c4b1c5
teoherman18 <ioana.herman@stud.ubbcluj.ro>
2021-10-23 00:46:13 +0300

    Problema 4

--- UI/console.py
+++ UI/console.py
@@ -1,3 +1,7 @@
+from Domain.librarie import to_string
+from Logic.CRUD import adauga_vanzare, sterge_vanzare, modifica_vanzare
+
+
 def print_menu():
     print("1. Adaugare vanzare")
     print("2. Stergere vanzare")
@@ -6,7 +10,47 @@ def print_menu():
     print("x. Iesire")
 
 
+def ui_adauga_vanzare(lista):
+    id = input("Dati id-ul: ")
+    titlu = input("Dati titul cartii: ")
+    gen = input("Dati genul cartii: ")
+    pret = float(input("Dati pretul cartii: "))
+    reducere = input("Ce fel de reducere a avut clientul? ")
+    return adauga_vanzare(id, titlu, gen, pret, reducere, lista)
+
+
+def ui_sterge_vanzare(lista):
+    id = input("Dati id-ul vanzarii de sters: ")
+    return sterge_vanzare(id, lista)
+
+
+def ui_modifica_vanzare(lista):
+    id = input("Dati id-ul vanzarii de modificat: ")
+    titlu = input("Dati noul titu: ")
+    gen = input("Dati noul gen: ")
+    pret = float(input("Dati noul pret: "))
+    reducere = input("Dati noul tip de reducere: ")
+    return modifica_vanzare(id, titlu, gen, pret, reducere, lista)
+
+
+def show_all(lista):
+    for vanzare in lista:
+        print(to_string(vanzare))
+
+
 def run_menu(lista):
     while True:
         print_menu()
         optiune = input("Dati optiunea: ")
+        if optiune == "1":
+            lista = ui_adauga_vanzare(lista)
+        elif optiune == "2":
+            lista = ui_sterge_vanzare(lista)
+        elif optiune == "3":
+            lista = ui_modifica_vanzare(lista)
+        elif optiune == "a":
+            show_all(lista)
+        elif optiune == "x":
+            break
+        else:
+            print("Optiune gresita! Reincercati: ")
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Tests.test_all import run_all_tests
+from UI.console import print_menu, run_menu
 
 
 def main():
+    lista = []
     run_all_tests()
+    run_menu(lista)
 
 if __name__ == '__main__':
     main()
